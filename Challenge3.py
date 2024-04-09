2f45b6e71b224b8c8c9e812116a5fe0f3199cdb3
Nicholas Kading <47288929+mxArkel86@users.noreply.github.com>
2024-04-09 16:13:10 -0700

    added A* algorithm

--- Challenge3.py
+++ Challenge3.py
@@ -2,6 +2,7 @@ import math
 import os
 import json
 import random
+from typing import Optional
 
 from dotenv import load_dotenv
 
@@ -53,11 +54,20 @@ def print_scores(client, topic_list, payload):
     print("")
 
 
+# The players game state is sent by the server, therefore before doing
+#   any processing, wait for the server to send the data and then give
+#   the go-ahead
 players_game_state = {}
-player_facing_direction = {}
 
 
 def print_game_state(client, topic_list, payload):
+    """
+    Save the game state retrieved from the server, and use it in another thread
+    to do processing on it.
+    :param client:
+    :param topic_list:
+    :param payload:
+    """
     global players_game_state
     player_name = topic_list[2]
     state = json.loads(payload)
@@ -66,6 +76,11 @@ def print_game_state(client, topic_list, payload):
 
 
 def display_game_board(game_state):
+    """
+    Display the game board in console in a user-friendly way
+    :param game_state:
+    :return:
+    """
     my_map = [[None for _ in range(5)] for _ in range(5)]
 
     playerpos = game_state["currentPosition"]
@@ -126,17 +141,27 @@ def sign_(num):
         return 0
 
 
-def compute_dist(pos1, pos2):
-    return math.sqrt(math.pow(pos1[0] - pos2[0], 2) + math.pow(pos1[1] - pos2[1], 2))
-
-
-class Path:
-    def __init__(self, pos, prev):
+# Class used for keeping track of the path taken to a coin in A*
+class Point:
+    def __init__(self, pos, prev, g, h):
         self.pos = pos
         self.prev_path = prev
+        self.g = g
+        self.h = h
+
+    def get_f(self):
+        return self.g + self.h
 
 
 def is_blocked(game_state, pos):
+    """
+    Check if there is a collision object at a specific position RELATIVE TO THE PLAYER
+    Collision objects: Walls, Teammates, Enemies, Map boundaries
+
+    :param game_state:
+    :param pos:
+    :return:
+    """
     block_objects = []
     block_objects.extend(game_state["walls"])
     block_objects.extend(game_state["teammatePositions"])
@@ -149,86 +174,85 @@ def is_blocked(game_state, pos):
         return True
 
     for obj in block_objects:
-        rel_pos = (obj[0] - playerpos[0], obj[1] - playerpos[1])
-
-        if rel_pos == pos:
+        if (obj[0] - playerpos[0], obj[1] - playerpos[1]) == pos:
             return True
 
     return False
 
 
-def rotate_right(dir):
-    if dir == (-1, 0):
-        return (0, 1)
-    elif dir == (0, 1):
-        return (1, 0)
-    elif dir == (1, 0):
-        return (0, -1)
-    else:
-        return (-1, 0)
+# Heuristic function for A*
+def calculate_h_value(pos: (int, int), dest: (int, int)):
+    return abs(pos[0] - dest[0]) + abs(pos[1] - dest[1])
 
 
-def compute_direction_to_coin(game_state):
-    all_coins = []
-    all_coins.extend(game_state["coin3"])
-    all_coins.extend(game_state["coin2"])
-    all_coins.extend(game_state["coin1"])
-    if len(all_coins) == 0:
-        return None
-
+def get_coin_path(game_state, target_coin) -> Optional[Point]:
+    """
+    Function that computes the POINT object that contains the path from the
+    start point to the end point.
+    :param game_state:
+    :param target_coin:
+    :return:
+    """
     playerpos = game_state["currentPosition"]
 
-    poi = [Path((0, 0), None)]
-    historical_pos = [(0, 0)]
-    path_out = None
+    frontier = [Point((0, 0), None, 0, 0)]
+    closed_path = {(0, 0): 0}
 
-    while True:
-        working_poi = poi[:]
-        poi.clear()
-        if path_out or len(working_poi) == 0:
-            break
+    while len(frontier) > 0:
+        working_frontier = sorted(frontier[:], key=lambda x: x.get_f())
+        frontier.clear()
+        for point in working_frontier:
+            for c in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                new_pos = (point.pos[0] + c[0], point.pos[1] + c[1])
+                game_pos = (playerpos[0] + point.pos[0] + c[0], playerpos[1] + point.pos[1] + c[1])
+                new_g = point.g + 1
+                new_h = calculate_h_value(game_pos, target_coin)
+                new_point = Point(new_pos, point, new_g, new_h)
 
-        for point in working_poi:
-            for coin in all_coins:
-                rel_pos = (coin[0] - playerpos[0], coin[1] - playerpos[1])
-                if rel_pos == point.pos:
-                    path_out = point
-                    break
+                # print(f"{new_pos} valid={is_valid(new_pos)}")
+                if game_pos[0] == target_coin[0] and game_pos[1] == target_coin[1]:
+                    return new_point
+                elif -2 <= new_pos[0] <= 2 and -2 <= new_pos[1] <= 2 and not is_blocked(game_state, new_pos) and (
+                        new_pos not in closed_path.keys() or new_point.get_f() < closed_path[new_pos]):
+                    frontier.append(Point(new_pos, point, new_g, new_h))
+                    closed_path[new_pos] = new_point.get_f()
 
-            candidates = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            for c in candidates:
-                rel_grid_pos = (point.pos[0] + c[0], point.pos[1] + c[1])
-                if not (-2 <= rel_grid_pos[0] <= 2 and -2 <= rel_grid_pos[1] <= 2):
-                    pass
+    return None
 
-                elif is_blocked(game_state, rel_grid_pos):
 
-                    pass
-                elif rel_grid_pos in historical_pos:
-                    pass
-
-                else:
-                    poi.append(Path(rel_grid_pos, point))
-
-                    historical_pos.append(rel_grid_pos)
-
-    if not path_out:
+def compute_direction_to_coin(game_state, target_coin):
+    """
+    Compute the path as a list of moves from the start point to
+    the end point.
+    :param game_state:
+    :param target_coin:
+    :return:
+    """
+    path = get_coin_path(game_state, target_coin)
+    if path is None:
         return None
 
+    # print("computed final coin position")
+
+    steps = []
     while True:
-        path_parent = path_out.prev_path
-        if path_parent.prev_path is None:
-            return path_out.pos
-        path_out = path_parent
+        path_parent = path.prev_path
+        if path_parent is None:
+            steps.reverse()
+            return steps
+
+        movement = (path.pos[0] - path_parent.pos[0], path.pos[1] - path_parent.pos[1])
+        steps.append(movement)
+        path = path_parent
 
 
-def rotate_facing_direction(dir, times):
-    for i in range(times):
-        dir = rotate_right(dir)
-    return dir
-
-
-def facing_direction_to_command(dir_coord):
+def moving_direction_to_command(dir_coord):
+    """
+    Convert a movement direction to a command that is understood by
+    the server.
+    :param dir_coord:
+    :return:
+    """
     my_map = {(-1, 0): "UP",
               (0, -1): "LEFT",
               (1, 0): "DOWN",
@@ -236,7 +260,30 @@ def facing_direction_to_command(dir_coord):
     return my_map[dir_coord]
 
 
+coin_fixation = {}
+
+
+def pick_new_coin(game_state, coins):
+    """
+    If a coin is unreachable or exits from view, find a new VALID COIN
+    to pick from.
+    :param game_state:
+    :param coins:
+    :return:
+    """
+    for potential_coin in coins:
+        direction_to_coin = compute_direction_to_coin(game_state, potential_coin)
+        if direction_to_coin is not None:
+            return potential_coin, direction_to_coin
+    return None, None
+
+
+# Use momentum parameter to catch coins quicker
+player_momentum = {}
+player_facing_direction = {}
+
 if __name__ == "__main__":
+    # def main():
     load_dotenv(dotenv_path='credentials.env')
 
     broker_address = os.environ.get('BROKER_ADDRESS')
@@ -250,8 +297,6 @@ if __name__ == "__main__":
     client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
     client.username_pw_set(username, password)
     client.connect(broker_address, broker_port)
-
-    time.sleep(0.5)
 
     client.on_subscribe = on_subscribe
     client.on_message = on_message
@@ -280,37 +325,86 @@ if __name__ == "__main__":
     while True:
         if game_finished:
             break
-        for player in players.keys():
+        for player in players.keys():  # do movement computations for each player
             if game_finished:
                 break
             while player not in players_game_state.keys() or players_game_state[player]["ready"] is False:
-                time.sleep(0.2)
+                time.sleep(0.2)  # if the game state is not ready for the player, wait until it is
 
             game_state = players_game_state[player]
+
             players_game_state[player]["ready"] = False
-            facing_direction = player_facing_direction[player]
 
             playerpos = game_state["currentPosition"]
 
-            direction_to_coin = compute_direction_to_coin(game_state)
+            all_coins = []
+            all_coins.extend(game_state["coin3"])
+            all_coins.extend(game_state["coin2"])
+            all_coins.extend(game_state["coin1"])
 
-            print(f"player game board [{player}]")
-            display_game_board(game_state)
+            print(f"{player} -------------")
+            print(game_state)
 
-            if direction_to_coin:
-                client.publish(f"games/{lobby_name}/{player}/move", facing_direction_to_command(direction_to_coin))
+            direction_to_coin = None
+            if player not in coin_fixation or coin_fixation[player] not in all_coins:
+                # give one specific coin for each player to "fixate" on. Don't keep switching coins because of
+                # context changes.
+                coin, direction_to_coin = pick_new_coin(game_state, all_coins)
+
+                if coin is not None:
+                    coin_fixation[player] = coin
+                else:
+                    if player in coin_fixation:
+                        del coin_fixation[player]
             else:
+                print(f"using existing coin")
+                direction_to_coin = compute_direction_to_coin(game_state, coin_fixation[player])
+
+            if direction_to_coin:  # valid coin on screen. Do A* pathing.
+                client.publish(f"games/{lobby_name}/{player}/move", moving_direction_to_command(direction_to_coin[0]))
+            else:  # do random pathing
+                if not is_blocked(game_state, player_facing_direction[player]):
+                    if player in player_momentum.keys():
+                        if player_momentum[player] > 0:
+                            player_momentum[player] -= 0.25
+                    else:
+                        player_momentum[player] = 1
+
+                    if random.random() < player_momentum[player]:
+                        # tend to move player over larger distances in order for easier traversal of the map.
+                        # (Pseudo-random motion)
+                        client.publish(f"games/{lobby_name}/{player}/move",
+                                       moving_direction_to_command(player_facing_direction[player]))
+                        continue
+
+                # if the forward direction is blocked, pick a new direction
                 candidates = [(1, 0), (-1, 0), (0, -1), (0, 1)]
                 valid_candidates = []
                 for dir in candidates:
                     if not is_blocked(game_state, dir):
                         valid_candidates.append(dir)
 
-                i = random.randint(0, len(valid_candidates) - 1)
-                facing_direction = valid_candidates[i]
-                client.publish(f"games/{lobby_name}/{player}/move", facing_direction_to_command(facing_direction))
+                if len(valid_candidates)==1:
+                    random_direction = valid_candidates[0]
+                    player_momentum[player] = 1  # reset momentum for new direction
+                    player_facing_direction[player] = random_direction
+
+                    client.publish(f"games/{lobby_name}/{player}/move",
+                                   moving_direction_to_command(random_direction))
+                elif len(valid_candidates)>1:
+                    i = random.randint(0, len(valid_candidates) - 1)
+                    random_direction = valid_candidates[i]  # pick from valid direction
+
+                    player_momentum[player] = 1  # reset momentum for new direction
+                    player_facing_direction[player] = random_direction
+
+                    client.publish(f"games/{lobby_name}/{player}/move",
+                                   moving_direction_to_command(random_direction))
+                else:
+                    print("player trapped. critical error")
 
             time.sleep(0.5)
+
     print("game finished")
     print("final scores: ", end='')
     for t, s in latest_scores.items():
